98bb7dfaec604940af829142ab11b5d2a68a51f7
Raj Magare <rajmagare007@gmail.com>
2025-11-20 15:37:57 +0530

    add background color changer project

---
+++ 02bgChanger/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("#212121");
+
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div
+          className="fixed flex flex-wrap justify-center 
+     bottom-12 inset-x-0 px-2 gap-2 rounded-xl"
+        >
+          <div className="bg-white flex flex-wrap justify-center gap-3 px-3 py-2 rounded-xl">
+            <button
+              className="outline-none px-4 py-1 rounded-xl text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+              onClick={() => setColor("red")}
+            >
+              red
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl text-white shadow-lg"
+              style={{ backgroundColor: "blue" }}
+              onClick={() => setColor("blue")}
+            >
+              blue
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl text-white shadow-lg"
+              style={{ backgroundColor: "green" }}
+              onClick={() => setColor("green")}
+            >
+              green
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl shadow-lg"
+              style={{ backgroundColor: "yellow" }}
+              onClick={() => setColor("yellow")}
+            >
+              yellow
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-xl text-white shadow-lg"
+              style={{ backgroundColor: "orange" }}
+              onClick={() => setColor("orange")}
+            >
+              orange
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
